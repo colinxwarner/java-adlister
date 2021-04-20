bc005a9918165730dcec253c3f3c086de238b876
Colin Warner <colin.x.warner@gmail.com>
2021-04-20 10:25:00 -0500

    Updating javabeans

---
+++ src/main/java/com/codeup/adlister/dao/Config.java
@@ -0,0 +1,7 @@
+package com.codeup.adlister.dao;
+
+public class Config {
+    public String getUrl() {return "jbdc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";}
+    public String getUser() {return "colin";}
+    public String getPassword() {return "MySql2021!!!";}
+}
